efc04e11bfe370eb1ba1e308b68855df8c70a3d7
Abdelali TAHIRI <tahiriabdo131@gmail.com>
2023-01-17 23:46:50 +0100

    add an other project & improve the the design

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".actions");
 const lines = document.querySelectorAll(".line");
 const x_mark = document.querySelector(".x-mark");
 
 hamburger.addEventListener("click", function () {
-    if (menu.style.display === "none") {
-        menu.style.display = "block";
-        x_mark.style.display = "block";
-        Array.from(lines).forEach(line => line.style.display = "none");
-    } else {
+    if (menu.style.display === "block") {
         menu.style.display = "none";
         x_mark.style.display = "none";
         Array.from(lines).forEach(line => line.style.display = "block");
+    }
+    else {
+        menu.style.display = "block";
+        x_mark.style.display = "block";
+        Array.from(lines).forEach(line => line.style.display = "none");
     }
 });
